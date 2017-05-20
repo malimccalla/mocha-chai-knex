0e2890ed95b38146674b378a41f8e5f6075059c3
Mali Michael <malimccalla@gmail.com>
2017-05-20 12:09:12 +0100

    Wrap get all shows in a describe block

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -30,25 +30,27 @@ describe('API Routes', function() {
     });
   });
 
-  it('should return all shows', function(done) {
-    chai.request(server)
-    .get('/api/v1/shows')
-    .end(function(err, res) {
-    res.should.have.status(200);
-    res.should.be.json;
-    res.body.should.be.a('array');
-    res.body.length.should.equal(4);
-    res.body[0].should.have.property('name');
-    res.body[0].name.should.equal('Suits');
-    res.body[0].should.have.property('channel');
-    res.body[0].channel.should.equal('USA Network');
-    res.body[0].should.have.property('genre');
-    res.body[0].genre.should.equal('Drama');
-    res.body[0].should.have.property('rating');
-    res.body[0].rating.should.equal(3);
-    res.body[0].should.have.property('explicit');
-    res.body[0].explicit.should.equal(false);
-    done();
+  describe('Get all shows', function() {
+    it('should get all shows', function(done) {
+      chai.request(server)
+      .get('/api/v1/shows')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json; // jshint ignore:line
+        res.body.should.be.a('array');
+        res.body.length.should.equal(4);
+        res.body[0].should.have.property('name');
+        res.body[0].name.should.equal('Suits');
+        res.body[0].should.have.property('channel');
+        res.body[0].channel.should.equal('USA Network');
+        res.body[0].should.have.property('genre');
+        res.body[0].genre.should.equal('Drama');
+        res.body[0].should.have.property('rating');
+        res.body[0].rating.should.equal(3);
+        res.body[0].should.have.property('explicit');
+        res.body[0].explicit.should.equal(false);
+        done();
+      });
     });
   });
 });
