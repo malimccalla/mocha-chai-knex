5676c18342684e5425f9b854ae38da6ae6396dc7
Mali Michael <malimccalla@gmail.com>
2017-05-20 12:06:08 +0100

    REFACTOR: rollback db and re-seed beforeEach

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,10 +4,32 @@ var chai = require('chai');
 var should = chai.should();
 var chaiHttp = require('chai-http');
 var server = require('../app');
+var knex = require('../db/knex');
 
 chai.use(chaiHttp);
 
 describe('API Routes', function() {
+
+  beforeEach(function(done) {
+    knex.migrate.rollback()
+    .then(function() {
+      knex.migrate.latest()
+      .then(function() {
+        return knex.seed.run()
+        .then(function() {
+          done();
+        });
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    knex.migrate.rollback()
+    .then(function() {
+      done();
+    });
+  });
+
   it('should return all shows', function(done) {
     chai.request(server)
     .get('/api/v1/shows')
